270a21440dda0aa95e1535c367aab114858a1afd
blackstar58 <blackstar58@gmail.com>
2016-11-17 16:11:33 -0800

    general script update

--- quote.js
+++ quote.js
@@ -1,9 +1,26 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://quotes.rest/qod.json",
-    }).then(function(data) {
-       $('.greeting-id').append(data.contents.quotes[0]);
-       $('.greeting-content').append(data.contents.length);
-    });
-});
+  $(document).ready(function() {
+      $("#getMessage").on("click",function(){
+       $.ajax({
+        url: "https://crossorigin.me/http://quotesondesign.com/wp-json/posts",
+        type: 'GET',
+        dataType: 'json',
+          contentType:"json",
+        success:function(data){
+      
+          var array = data;
+            console.log(array);
+            $(".message").html(array[0].content);
+           
+            $(".message").val(array[0].ID);
+            $("#content").val(array[0].content);
+            $("#link").val(array[0].link);
+            $("#title").val(array[0].title);
+        },
+                error: function(err) {
+            var data = $.parseJSON(err.responseText);
+            showErrorModal(data.errorMessage);
+        }
 
+   });
+});
+      });
