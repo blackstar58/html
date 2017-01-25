776ffacaf24e3ed3bf69454a5586fa8e27fb228a
blackstar58 <blackstar58@gmail.com>
2017-01-25 15:58:44 -0800

    Jan 22nd Update

--- wiki.js
+++ wiki.js
@@ -1,49 +1,39 @@
 $(document).ready(function(){
+  $("#myDIV").hide();
     
-    $("#search").on("click",function(){     
+$("#search").on("click",function(){     
     var searchQuery = document.getElementById("searchbox").value;
     console.log(searchQuery);    
-    
-    
-$.ajax({
-    type: 'GET',
-        url: ''
-    
-    
-    
-    
-    
-    
-})
+        var title = [];
+        var links = [];
+        var description = [];   
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&generator=search&search="+searchQuery+"&format=json&origin=*&gsrprop=snippet&prop=info&inprop=url",function(data){
+        console.log(data);
+        console.log(data[1][0]);
+        console.log(data[1].length)
+
+        for (var i = 0;i < data[1].length;i++){
+            
+           title.push(data[1][i]);
+           description.push(data[2][i]);
+           links.push(data[3][i]);
+        var table = document.getElementById("myTable");
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);        
+        cell1.innerHTML = data[1][i];
+        cell2.innerHTML = data[2][i];
+        cell3.innerHTML = data[3][i];        
+
+            
+        }
+     })
         
-        
-    })
-})
-
-
-
-
-
-/*$.ajax( {
-    type: 'GET',
-        url: 'https://api.openweathermap.org/data/2.5/weather',
-        data: {lat:lat,
-               lon:long,
-               appid:'c163954ce735926f7d6eea7476f5c959'},
-        crossDomain:true, 
-        dataType: 'json',
     
+        console.log(title);
+        console.log(links);
+        console.log(description);
+           
     
-    url: remoteUrlWithOrigin,
-    data: queryData,
-    dataType: 'json',
-    type: 'POST',
-    headers: { 'wikisearch-free-code': 'wikipedia/1.0' },
-    success: function(data) {
-       
-  // https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=car&format=json&gsrprop=snippet&prop=info&inprop=url     
-        
-        
-   // }
-} ); */
-
+     } )})
