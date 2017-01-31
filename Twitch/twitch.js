912516827036d2c1f0b9d58229d9a9b8e50572c0
blackstar58 <blackstar58@gmail.com>
2017-01-30 16:19:28 -0800

    Twitch functional Update

---
+++ Twitch/twitch.js
@@ -0,0 +1,20 @@
+    
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","brunofin","comster404"]
+
+var clientID = "sbp1wnku2j32dtnjj4qefhslopxq8s";
+
+$(document).ready(function() {
+    
+ $.getJSON("https://api.twitch.tv/kraken/streams/ESL_SC2?client_id=sbp1wnku2j32dtnjj4qefhslopxq8s",function(data){
+
+console.log(data); 
+console.log(data.stream);     
+console.log(data._links);
+console.log(data.stream.game);     
+document.getElementById("thumbnail_image").src = data.stream.channel.profile_banner; 
+     
+     })  
+})
+
+    
+    
