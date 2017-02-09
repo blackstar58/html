d523ab25ffec4ad6cd7414c14ee80707bea5e598
blackstar58 <blackstar58@gmail.com>
2017-02-08 16:09:29 -0800

    Updated

--- Twitch/twitch.js
+++ Twitch/twitch.js
@@ -4,15 +4,29 @@ var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck"
 var clientID = "sbp1wnku2j32dtnjj4qefhslopxq8s";
 
 $(document).ready(function() {
-    
- $.getJSON("https://api.twitch.tv/kraken/streams/ESL_SC2?client_id=sbp1wnku2j32dtnjj4qefhslopxq8s",function(data){
 
+for (var i = 0;i < channels.length; i++){
+    
+    //console.log(channels[i]);
+var lowVariable = channels[i];
+    console.log(channels[i]);
+    
+$.getJSON("https://api.twitch.tv/kraken/streams/"+channels[i]+"?client_id=sbp1wnku2j32dtnjj4qefhslopxq8s",function(data){
 console.log(data); 
-console.log(data.stream);     
-console.log(data._links);
-console.log(data.stream.game);     
-document.getElementById("Title").textContent = data.stream.game;     
+if (data.stream == null){
+    console.log("No Stream");
+    console.log(data.stream);
+    console.log(data._links.channel);
+} else {
+    console.log("Steam Available");
+    console.log(data.stream);
+    console.log(data._links.channel);
+}       
+   
      })  
+    
+}    
+    
 })
 
     
