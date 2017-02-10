838083389d63457da46660d4894f6979e8f771a5
blackstar58 <blackstar58@gmail.com>
2017-02-09 16:38:05 -0800

    Update

--- Twitch/twitch.js
+++ Twitch/twitch.js
@@ -9,22 +9,31 @@ for (var i = 0;i < channels.length; i++){
     
     //console.log(channels[i]);
 var lowVariable = channels[i];
-    console.log(channels[i]);
+  //  console.log(channels[i]);
+var allChannels = [];    
     
 $.getJSON("https://api.twitch.tv/kraken/streams/"+channels[i]+"?client_id=sbp1wnku2j32dtnjj4qefhslopxq8s",function(data){
 console.log(data); 
 if (data.stream == null){
+    allChannels[i] = [];
+    allChannels[i][0] = channels[i];
+    allChannels[i][1] = data.stream;
+    allChannels[i][2] = "No Stream";
     console.log("No Stream");
-    console.log(data.stream);
-    console.log(data._links.channel);
+   // console.log(data.stream);
+    //console.log(data._links.channel);
 } else {
+    allChannels[i][0] = channels[i];
+    allChannels[i][1] = data.stream;
+    allChannels[i][2] = "Steam Available";
     console.log("Steam Available");
-    console.log(data.stream);
-    console.log(data._links.channel);
+    //console.log(data.stream);
+    //console.log(data._links.channel);
 }       
    
      })  
-    
+    console.log(allChannels);
+
 }    
     
 })
