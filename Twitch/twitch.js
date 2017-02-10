cd7ebb8312719c01f38bb2b7d379b095e77a3ce5
blackstar58 <blackstar58@gmail.com>
2017-02-10 14:44:55 -0800

    Update

--- Twitch/twitch.js
+++ Twitch/twitch.js
@@ -2,40 +2,50 @@
 var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","brunofin","comster404"]
 
 var clientID = "sbp1wnku2j32dtnjj4qefhslopxq8s";
+var allChannels = [];
+var feedReturn = [];
 
 $(document).ready(function() {
+    
+    
+   
 
 for (var i = 0;i < channels.length; i++){
-    
-    //console.log(channels[i]);
-var lowVariable = channels[i];
-  //  console.log(channels[i]);
-var allChannels = [];    
+
+    allChannels[i] = new Array(3);
+     allChannels[i][0] = channels[i]
+     
+     
+    console.log(channels[i]);
     
 $.getJSON("https://api.twitch.tv/kraken/streams/"+channels[i]+"?client_id=sbp1wnku2j32dtnjj4qefhslopxq8s",function(data){
-console.log(data); 
+
+    $.each(data,function(i){
+
+        console.log(channels[i]);
+        feedReturn.push(data[i]);
+        feedReturn.i.push("No Stream");
+        
+        
+    })
+    
+    console.log(data); 
 if (data.stream == null){
-    allChannels[i] = [];
-    allChannels[i][0] = channels[i];
-    allChannels[i][1] = data.stream;
-    allChannels[i][2] = "No Stream";
+  
     console.log("No Stream");
-   // console.log(data.stream);
-    //console.log(data._links.channel);
+
 } else {
-    allChannels[i][0] = channels[i];
-    allChannels[i][1] = data.stream;
-    allChannels[i][2] = "Steam Available";
+    
     console.log("Steam Available");
-    //console.log(data.stream);
-    //console.log(data._links.channel);
+
 }       
    
      })  
-    console.log(allChannels);
+
 
 }    
-    
+        console.log(allChannels);
+        console.log(feedReturn);
 })
 
     
