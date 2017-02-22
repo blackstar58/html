a67a1e92e51fc1c0d1d357e9f7b8196fc00f02b5
blackstar58 <blackstar58@gmail.com>
2017-02-21 16:06:25 -0800

    Feb 21 Update

--- Twitch/twitch.js
+++ Twitch/twitch.js
@@ -1,52 +1,72 @@
-    
+
 var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","brunofin","comster404"]
 
 var clientID = "sbp1wnku2j32dtnjj4qefhslopxq8s";
-var allChannels = [];
+
 var feedReturn = [];
 
-$(document).ready(function() {
+
+channels.forEach(function(element){
     
+    console.log(element)
+    $.getJSON("https://api.twitch.tv/kraken/streams/"+element+"?client_id=sbp1wnku2j32dtnjj4qefhslopxq8s",function(data){
+      
+        if(data.stream === null){
+            
+           feedReturn.push(["Offline",data._links.channel,element]); 
     
-   
-
-for (var i = 0;i < channels.length; i++){
-
-    allChannels[i] = new Array(3);
-     allChannels[i][0] = channels[i]
-     
-     
-    console.log(channels[i]);
-    
-$.getJSON("https://api.twitch.tv/kraken/streams/"+channels[i]+"?client_id=sbp1wnku2j32dtnjj4qefhslopxq8s",function(data){
-
-    $.each(data,function(i){
-
-        console.log(channels[i]);
-        feedReturn.push(data[i]);
-        feedReturn.i.push("No Stream");
+        } else if (data.stream === undefined) {
+        feedReturn.push(["Account Closed",element]); 
         
-        
-    })
-    
-    console.log(data); 
-if (data.stream == null){
-  
-    console.log("No Stream");
+        }else {            
+            feedReturn.push(["Online",data._links.channel,element]);
+            
+        }
 
-} else {
-    
-    console.log("Steam Available");
-
-}       
-   
      })  
-
-
-}    
-        console.log(allChannels);
-        console.log(feedReturn);
 })
 
+function buildHtmlTable() {
+     var columns = addAllColumnHeaders(feedReturn);
+ 
+     for (var i = 0 ; i < myList.length ; i++) {
+         var row$ = $('<tr/>');
+         for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+             var cellValue = myList[i][columns[colIndex]];
+ 
+             if (cellValue == null) { cellValue = ""; }
+ 
+             row$.append($('<td/>').html(cellValue));
+         }
+         $("#excelDataTable").append(row$);
+     }
+ }
+
+ function addAllColumnHeaders(feedReturn)
+ {
+     var columnSet = [];
+     var headerTr$ = $('<tr/>');
+ 
+     for (var i = 0 ; i < myList.length ; i++) {
+         var rowHash = myList[i];
+         for (var key in rowHash) {
+             if ($.inArray(key, columnSet) == -1){
+                 columnSet.push(key);
+                 headerTr$.append($('<th/>').html(key));
+             }
+         }
+     }
+     $("#excelDataTable").append(headerTr$);
+ 
+     return columnSet;
+ }
     
+    console.log(feedReturn);
+
+ $(document).ready(function() {     
+
+
+})
+    
+
     
