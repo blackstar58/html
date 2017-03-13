36bb062214a79e74f0091daf3d7c14f1a8b6d212
blackstar58 <blackstar58@gmail.com>
2017-03-13 16:02:34 -0700

    Calculator
    Updated the calculator

---
+++ cal/cal.js
@@ -0,0 +1,23 @@
+var currentValue;
+var totalValue;
+var calcChain = [];
+
+for (var = 0; i < calcChain.length;i++){
+    
+    switch(var[i]){
+            
+        case '+':
+            
+            
+            
+            
+            
+            
+    }
+    
+    
+    
+}
+
+
+
