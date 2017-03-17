428793bb85a9785382e8d2b92bca723b12552b3a
blackstar58 <blackstar58@gmail.com>
2017-03-17 15:57:31 -0700

    March 17th Update

--- cal/cal.js
+++ cal/cal.js
@@ -2,22 +2,48 @@ var currentValue;
 var totalValue;
 var calcChain = [];
 
-for (var = 0; i < calcChain.length;i++){
+
+
+ $(document).ready(function() {     
+
+ /*    document.getElementById('1').addEventListener("click",displayNum);
+  //   document.getElementById('2').addEventListener("click",displayNum);
+     document.getElementById('3').addEventListener("click",displayNum);
+     document.getElementById('4').addEventListener("click",displayNum);
+     document.getElementById('5').addEventListener("click",displayNum);
+     document.getElementById('6').addEventListener("click",displayNum);
+     document.getElementById('7').addEventListener("click",displayNum);
+     document.getElementById('9').addEventListener("click",displayNum);
+     document.getElementById('zero').addEventListener("click",displayNum);
+     document.getElementById('multiply').addEventListener("click",displayNum);
+     document.getElementById('division').addEventListener("click",displayNum);
+     document.getElementById('minus').addEventListener("click",displayNum);
+     document.getElementById('plus').addEventListener("click",displayNum);
+     document.getElementById('equals').addEventListener("click",displayNum); */
+     
+     
+     
+   var theParent = document.querySelector("#theParent");
+     
     
-    switch(var[i]){
-            
-        case '+':
-            
-            
-            
-            
-            
-            
-    }
-    
-    
-    
+     
+  for (var i = 0; i < theParent.children.length; i++) {
+    var childElement = theParent.children[i];
+    childElement.addEventListener('click', doSomething, false);
 }
-
-
-
+ 
+function doSomething(e) {
+    var clickedItem = e.target.id;
+     console.log(clickedItem);
+     panel.innerHTML = clickedItem;
+   }   
+   
+     
+     
+     
+    
+ 
+     
+     
+})
+    
