13112a78875c0cf2fd15f3a3691c210d95d60288
blackstar58 <blackstar58@gmail.com>
2017-03-21 16:02:43 -0700

    latest update

--- cal/cal.js
+++ cal/cal.js
@@ -6,21 +6,6 @@ var calcChain = [];
 
  $(document).ready(function() {     
 
- /*    document.getElementById('1').addEventListener("click",displayNum);
-  //   document.getElementById('2').addEventListener("click",displayNum);
-     document.getElementById('3').addEventListener("click",displayNum);
-     document.getElementById('4').addEventListener("click",displayNum);
-     document.getElementById('5').addEventListener("click",displayNum);
-     document.getElementById('6').addEventListener("click",displayNum);
-     document.getElementById('7').addEventListener("click",displayNum);
-     document.getElementById('9').addEventListener("click",displayNum);
-     document.getElementById('zero').addEventListener("click",displayNum);
-     document.getElementById('multiply').addEventListener("click",displayNum);
-     document.getElementById('division').addEventListener("click",displayNum);
-     document.getElementById('minus').addEventListener("click",displayNum);
-     document.getElementById('plus').addEventListener("click",displayNum);
-     document.getElementById('equals').addEventListener("click",displayNum); */
-     
      
      
    var theParent = document.querySelector("#theParent");
@@ -37,9 +22,70 @@ function doSomething(e) {
     
      console.log(e.toElement.id);
     
+    var value = e.toElement.id;
      panel.innerHTML = e.toElement.id;
     
-    
+    switch(value){
+        case '1':
+            panel.innerHTML = '1';
+            break;
+        case '2':
+            panel.innerHTML = '2';
+            break;
+        case '3':
+            panel.innerHTML = '3';
+            break;
+        case '4':
+            panel.innerHTML = '4';
+            break;
+        case '5':
+            panel.innerHTML = '5';
+            break;
+        case '6':
+            panel.innerHTML = '6';
+            break;
+        case '7':
+            panel.innerHTML = '7';
+            break;
+        case '8':
+            panel.innerHTML = '8';
+            break;
+        case '9':
+            panel.innerHTML = '9';
+            break;
+        case '0':
+            panel.innerHTML = '0';
+            break;
+        case 'divison':
+            panel.innerHTML = '&bsol;';
+            break;
+        case 'multiply':
+            panel.innerHTML = '&ast;';
+            break;
+        case 'plus':
+            panel.innerHTML = '&plus;';
+            break;
+        case 'minus':
+            panel.innerHTML= '&minus;'
+            break;
+        case 'equals':
+            panel.innerHTML = '&equals;';
+            break;
+        case 'period':
+            panel.innerHTML = '&period;';
+            break;
+        case 'AC':
+            panel.innerHTML
+        default:
+            panel.innerHTML = '0';
+            
+        
+            
+            
+            
+            
+            
+    }
     
     
    }   
