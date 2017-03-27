b25b25b25559d0a5846f4647abd10160473f3684
blackstar58 <blackstar58@gmail.com>
2017-03-27 16:21:39 -0700

    Final Update
    All Changes made

--- cal/cal.js
+++ cal/cal.js
@@ -18,57 +18,56 @@ function doSomething(e) {
     panel.innerHTML = e.toElement.id; 
     switch(value){
         case '1':
-            currentValue = checkNull(currentValue,'1');
+            currentValue = checkNull(currentValue, '1');
             console.log(currentValue);
             console.log(calcChain);
              panel.innerHTML = currentValue;
             break;
         case '2':
             
-            currentValue = checkNull(currentValue,'2');
+            currentValue = checkNull(currentValue, '2');
             console.log(calcChain);
             console.log(currentValue);
             panel.innerHTML = currentValue;
             break;
         case '3':
-            currentValue = checkNull(currentValue,'3');
+            currentValue = checkNull(currentValue, '3');
             panel.innerHTML = currentValue;
             console.log(calcChain);
             break;
         case '4':
-            currentValue = checkNull(currentValue,'4');
+            currentValue = checkNull(currentValue, '4');
             panel.innerHTML = currentValue;
             console.log(currentValue);
             console.log(calcChain);
             break;
         case '5':
-            currentValue = checkNull(currentValue,'5');
+            currentValue = checkNull(currentValue, '5');
             panel.innerHTML = currentValue;
             console.log(currentValue);
             console.log(calcChain);
             break;
         case '6':
-            currentValue = checkNull(currentValue,'6');
+            currentValue = checkNull(currentValue, '6');
             panel.innerHTML = currentValue;
             console.log(currentValue);
             console.log(calcChain);
             break;
         case '7':
-            currentValue = checkNull(currentValue,'7');
+            currentValue = checkNull(currentValue, '7');
             panel.innerHTML = currentValue;
            
             console.log(currentValue);
             console.log(calcChain);
             break;
         case '8':
-            currentValue = checkNull(currentValue,'8');
+            currentValue = checkNull(currentValue, '8');
             panel.innerHTML = currentValue;
-            
             console.log(currentValue);
             console.log(calcChain);
             break;
         case '9':
-            currentValue = checkNull(currentValue,'9');
+            currentValue = checkNull(currentValue, '9');
             panel.innerHTML = currentValue;
             
             console.log(currentValue);
@@ -76,7 +75,7 @@ function doSomething(e) {
             break;
         case 'zero':
             
-            currentValue = checkNull(currentValue,'0');
+            currentValue = checkNull(currentValue, '0');
             panel.innerHTML = currentValue;
             
             console.log(currentValue);
@@ -86,10 +85,11 @@ function doSomething(e) {
              if(currentValue == null){
                 panel.innerHTML = '0';
             } else{
-                calcChain.push(currentValue);
-            panel.innerHTML = '0';
-            calcChain.push('/');
-            console.log(calcChain);
+            calcChain.push(currentValue);
+            calcChain.push('/');   
+             panel.innerHTML = '0';
+                currentValue = null; 
+            console.log(calcChain);  
             break;
             }
         case 'multiply':
@@ -97,7 +97,7 @@ function doSomething(e) {
             if(currentValue == null){
                 panel.innerHTML = '0';
             } else{
-                calcChain.push(currentValue);
+            calcChain.push(currentValue);
             calcChain.push('*');
             panel.innerHTML = '0';
             currentValue = null;
@@ -123,6 +123,7 @@ function doSomething(e) {
             calcChain.push(currentValue);
             panel.innerHTML= '0';
             calcChain.push('-');
+            currentValue = null;    
             console.log(calcChain);
             break;
             }
@@ -130,25 +131,40 @@ function doSomething(e) {
             if(currentValue == null){
                 panel.innerHTML = '0';
             } else {
-                calcChain.push(currentValue);  
-                calEngine(calcChain);
+                calcChain.push(currentValue); 
+                console.log(calcChain);
+                var finalresult = calEngine(calcChain);
+                panel.innerHTML = finalresult;
+                calcChain = [];
+                currentValue = null;
                break; 
             }
-        case 'period':
+        case 'plusmn':
+            if(currentValue == null){
+                panel.innerHTML = '0';
+            } else {
+                currentValue = currentValue * -1;
+                console.log(currentValue);
+                panel.innerHTML = currentValue;
+                break;
+            } 
             
+        
+        
+        
+        case 'period':
             if(currentValue.indexOf('.') == -1){
                 currentValue = checkNull(currentValue,'.');    
             } else {
                 panel.innerHTML = currentValue;
             }
-            
             panel.innerHTML = currentValue;
             console.log(currentValue);
-            
             console.log(calcChain);
             break;
         case 'AC':
             calcChain = [];
+            currentValue = null;
             console.log(calcChain);
             panel.innerHTML = '0';
             break;
@@ -167,23 +183,66 @@ function doSomething(e) {
         if (currentValue == null){
             return currentValue = value;
         } else {
+            if (currentValue == '0'){
+                
+                currentValue = value;
+                return currentValue;
+                
+            } else {
+                
                 return currentValue += value;
             }
+               
+            }
             
-        }
+        }//end of checkNull Function
     
     function calEngine(arr){
-        
         var total = 0;
-        
-        
-        for(var j =0;j < arr.length;j++){
-            
-            console.log(arr[j]);
-            console.log("update")
-            
-        }
        
+        for(var j =0;j < arr.length;j++){
+            if(j == 0){
+                
+                var t = parseInt(arr[j]);
+                       total += t; 
+                console.log(total);
+            } else {
+                
+                var action = null;
+                if(arr[j] == '+'){
+
+                    action = parseInt(arr[j + 1]);
+                    console.log("value of s: " + action);
+                    total += action;  
+                    console.log("value of total in add: " + total);
+                    j++;
+                } else if (arr[j] == '-'){
+                    
+                    action = parseInt(arr[j + 1]);
+                    console.log("value of m in mimus: " + action);
+                    total = total - action;
+                    console.log("value of total in minus: " + total);
+                    j++
+                    
+                }  else if (arr[j] == '*'){
+                    action = parseInt(arr[j + 1]);
+                    console.log("value of n in multiply: " + action);
+                    total = total * action;
+                    console.log("value of total in multiply: " + total);
+                    j++;
+                } else if (arr[j] == '/'){
+    
+                    action = parseInt(arr[j + 1]);
+                    console.log("value of n in divide: " + action);
+                    total = total / action;
+                    console.log("value of total in divide: " + total);
+                    j++;
+                    
+                }
+            }
+            
+        }//End of cal Engine
+       return total;
         
     }
     
