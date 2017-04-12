1f9e6b770093ed919269fc6abc14802b7d309754
blackstar58 <blackstar58@gmail.com>
2017-04-12 16:39:56 -0700

    April 12th update

--- pomodoro/clock.js
+++ pomodoro/clock.js
@@ -1,121 +1,62 @@
 $(document).ready(function (){   
 	
 
-var timelimit = 35
-var pomMin = new Date().getTime() 
-var countDownDate = pomMin + (timelimit * 1000 * 60)
-
-$("#plus").on("click",function(){  
+function myCounter(timelimit){
     
+    var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+    document.getElementById("demo").innerHTML = minutes + " m"  + " : "+ seconds + " s";
+}, 1000);
+}
+    
+$("#plus").on("click",function(){  
     timelimit = timelimit + 1
     console.log(timelimit)
     document.getElementById("value").innerHTML = timelimit; 
 })
-
     $("#minus").on("click",function(){  
-
     timelimit = timelimit - 1
     console.log(timelimit)
     document.getElementById("value").innerHTML = timelimit; 
 })
-
-
-
   $("#long").on("click",function(){
        timelimit = 15
 	  console.log(timelimit)
 	  pomMin = new Date().getTime() 
-	  countDownDate = pomMin + (timelimit * 1000 * 60)
+	  countDownDate = pomMin + (timelimit * 1000 * 60) 
+            myCounter(timelimit);
 
-      
-var x = setInterval(function() {
-
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = minutes + " m"  + " : "+ seconds + " s";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
-	   }   
-     
+	   }    
 )
   
-	  
   $("#short").on("click",function(){
        timelimit = 5
 	  console.log(timelimit)
 	  pomMin = new Date().getTime() 
-	  countDownDate = pomMin + (timelimit * 1000 * 60)
-	  
-var x = setInterval(function() {
-
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = minutes + " m"  + " : "+ seconds + " s";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-	
-	    
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
+	  countDownDate = pomMin + (timelimit * 1000 * 60)	  
+      myCounter(timelimit);
 	   }   
-     
 )	
 	
 
   $("#restart").on("click",function(){
       timelimit = parseInt(document.getElementById("value").innerHTML)
 	  console.log(timelimit)
-	  pomMin = new Date().getTime() 
-	  countDownDate = pomMin + (timelimit * 1000 * 60)
-
+	 pomMin = new Date().getTime() 
+	countDownDate = pomMin + (timelimit * 1000 * 60)
           
     if(document.getElementById("restart").innerHTML == "Start"){
-        document.getElementById("restart").innerHTML = "Restart"
-        
-        
+        document.getElementById("restart").innerHTML = "Restart"   
+        myCounter(timelimit);
     } else {
-        
          document.getElementById("restart").innerHTML = "Start"
-
+        myCounter(timelimit);
     }
-      
-var x = setInterval(function() {
-
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = minutes + " m"  + " : "+ seconds + " s";
-    
-    if(distance < 0){
-        
-        console.log(timelimit)
-        x = setInterval(loop,1000);
-        
-    }
-
-}, 1000);
-      
-	   }   
-     
-)	
-    
+	   }       
+)	//end of restart function
     //end of script
 	   }   
      
