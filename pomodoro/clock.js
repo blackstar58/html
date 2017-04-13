76e08f8a5b803eaab919cd087ec10cc834742401
blackstar58 <blackstar58@gmail.com>
2017-04-13 16:33:50 -0700

    April 13th - Update

--- pomodoro/clock.js
+++ pomodoro/clock.js
@@ -1,60 +1,71 @@
 $(document).ready(function (){   
 	
+    var x = null;
 
-function myCounter(timelimit){
-    
-    var x = setInterval(function() {
+function myCounter(countDownDate){
+    clearInterval(x)
+    x = setInterval(function() {
+        
     var now = new Date().getTime();
     var distance = countDownDate - now;
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000)
     document.getElementById("demo").innerHTML = minutes + " m"  + " : "+ seconds + " s";
+        
+    if(distance < 0){
+        clearInterval(x)
+        //myCounter(countDownDate)
+        console.log("Print Field")
+    }    
+        
 }, 1000);
+    
 }
     
-$("#plus").on("click",function(){  
+$("#plus").on("click",function(){
+    timelimit = parseInt(document.getElementById("value").innerHTML)
     timelimit = timelimit + 1
-    console.log(timelimit)
     document.getElementById("value").innerHTML = timelimit; 
 })
     $("#minus").on("click",function(){  
+    timelimit = parseInt(document.getElementById("value").innerHTML)
     timelimit = timelimit - 1
-    console.log(timelimit)
     document.getElementById("value").innerHTML = timelimit; 
 })
   $("#long").on("click",function(){
-       timelimit = 15
-	  console.log(timelimit)
+      timelimit = 15
 	  pomMin = new Date().getTime() 
 	  countDownDate = pomMin + (timelimit * 1000 * 60) 
-            myCounter(timelimit);
+      myCounter(countDownDate);
+
 
 	   }    
 )
   
   $("#short").on("click",function(){
        timelimit = 5
-	  console.log(timelimit)
 	  pomMin = new Date().getTime() 
 	  countDownDate = pomMin + (timelimit * 1000 * 60)	  
-      myCounter(timelimit);
+      myCounter(countDownDate);
 	   }   
 )	
 	
 
   $("#restart").on("click",function(){
       timelimit = parseInt(document.getElementById("value").innerHTML)
-	  console.log(timelimit)
 	 pomMin = new Date().getTime() 
 	countDownDate = pomMin + (timelimit * 1000 * 60)
           
     if(document.getElementById("restart").innerHTML == "Start"){
+        
         document.getElementById("restart").innerHTML = "Restart"   
-        myCounter(timelimit);
+        
     } else {
-         document.getElementById("restart").innerHTML = "Start"
-        myCounter(timelimit);
+        
+        document.getElementById("restart").innerHTML = "Start"
+  
     }
+      myCounter(countDownDate);
 	   }       
 )	//end of restart function
     //end of script
