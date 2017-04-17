79717aa4d85174d6093416e83244934118c923f6
blackstar58 <blackstar58@gmail.com>
2017-04-17 16:40:35 -0700

    April 17th 2017 - Update

--- ticktoe/tick.js
+++ ticktoe/tick.js
@@ -7,33 +7,41 @@ $('#myModal').modal('show');
 
 
 $(document).ready(function (){  
-console.log('document is ready')
 	
-console.log('print on load')
 var userSelection = null;
 var computerSelection = null;
 var tickMatrix = [];
 	
 $('#selectX').on("click",function(){	
-	console.log("This is X");
+	
 	userSelection = 'X';
 	computerSelection = 'O';
-	console.log(userSelection)
-	console.log(computerSelection)
+	console.log("User selects: " + userSelection)
+	console.log("Computer selects: " + computerSelection)
+    $('#myModal').modal('hide');
+    theGame(userSelection,computerSelection);
 	})//end of select X function
 	
 $('#selectO').on("click",function(){		
-	console.log("This is O");
+	
 	userSelection = 'O';
 	computerSelection = 'X';
-	console.log(userSelection)
-	console.log(computerSelection)
+    console.log("User selects: " + userSelection)
+	console.log("Computer selects: " + computerSelection)
+    $('#myModal').modal('hide');
+    theGame(userSelection,computerSelection);
 	})//end of select O function	
-	
 
-	
-
-
+    
+function theGame(userSelection,computerSelection){
+    
+    console.log(userSelection + ":" + computerSelection);
+    
+    
+}
+    
+    
+    
 
 })//document ready
 
