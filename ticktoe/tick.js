41972c964762b25baabf6c81bd971c17bb4f052a
blackstar58 <blackstar58@gmail.com>
2017-04-18 15:45:55 -0700

    Update April 18th 2017
    Latest Updates

--- ticktoe/tick.js
+++ ticktoe/tick.js
@@ -11,6 +11,7 @@ $(document).ready(function (){
 var userSelection = null;
 var computerSelection = null;
 var tickMatrix = [];
+var whosMove = null;    
 	
 $('#selectX').on("click",function(){	
 	
@@ -19,41 +20,63 @@ $('#selectX').on("click",function(){
 	console.log("User selects: " + userSelection)
 	console.log("Computer selects: " + computerSelection)
     $('#myModal').modal('hide');
-    theGame(userSelection,computerSelection);
+    whosMove = 0;
+    console.log(whosMove);
 	})//end of select X function
 	
 $('#selectO').on("click",function(){		
 	
 	userSelection = 'O';
 	computerSelection = 'X';
+    whosMove = 1;
     console.log("User selects: " + userSelection)
 	console.log("Computer selects: " + computerSelection)
     $('#myModal').modal('hide');
-    theGame(userSelection,computerSelection);
+    console.log(whosMove);
 	})//end of select O function	
+        
 
-  var theParent = document.querySelector("#workArea");
-   
+var theParent = document.querySelector("#workArea");
     for (var i = 0; i < theParent.children.length; i++) {
     var childElement = theParent.children[i];
-      
-    childElement.addEventListener('click', doSomething, false);
-}
+    childElement.addEventListener('click', clickedElement, false);
+}//end of click listener
     
-function doSomething(e){
+function clickedElement(e){
 	var value = e.srcElement.id;
-	console.log(value);
-	
-}	
-	
-	
-function theGame(userSelection,computerSelection){
-    console.log(userSelection + ":" + computerSelection);
+   // console.log(userSelection);
+    //console.log(computerSelection);
+    e.path[0].innerHTML = userSelection;
+    theGame(value);    
+}	//end of clickElement Modifier
 	
 	
-    
-    
-}
+function theGame(value){
+     switch(value){
+        case '1':
+             console.log("value of whosMoveL: " + whosMove);
+             console.log("value of userSelection in the Game: " + userSelection)      
+             if(whosMove == 0){     
+                tickMatrix.push('1',userSelection); 
+                console.log(tickMatrix);
+                whosMove = 1; 
+	console.log("whois 0 " + computerSelection) 
+             } else if(whosMove == 1) {
+                 tickMatrix.push('1',computerSelection)
+                 console.log(tickMatrix);
+                 whosMove = 0;
+             }
+            break;//end of case 1
+             
+     
+             
+             
+             
+             
+             
+             
+                 }//end of switch statement
+}//end of the gamefunction
     
     
     
