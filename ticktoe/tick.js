fb18a770bb980d8b5b08cdc2074415ab8fa272f1
blackstar58 <blackstar58@gmail.com>
2017-04-20 16:43:20 -0700

    April 20th Update
    Update to tick tac toe

--- ticktoe/tick.js
+++ ticktoe/tick.js
@@ -11,7 +11,8 @@ $(document).ready(function (){
 var userSelection = null;
 var computerSelection = null;
 var tickMatrix = [];
-var whosMove = null;    
+var whosMove = null; 
+var tempArray = [];
 	
 $('#selectX').on("click",function(){	
 	
@@ -25,14 +26,12 @@ $('#selectX').on("click",function(){
 	})//end of select X function
 	
 $('#selectO').on("click",function(){		
-	
 	userSelection = 'O';
 	computerSelection = 'X';
     whosMove = 1;
     console.log("User selects: " + userSelection)
 	console.log("Computer selects: " + computerSelection)
     $('#myModal').modal('hide');
-    console.log(whosMove);
 	})//end of select O function	
         
 
@@ -46,18 +45,69 @@ function clickedElement(e){
 	var value = e.srcElement.id;
    // console.log(userSelection);
     //console.log(computerSelection);
-    e.path[0].innerHTML = userSelection;
-    theGame(value);    
+    e.path[0].innerHTML = userSelection;    
 }	//end of clickElement Modifier
+
+    
+var theMatrixParent = document.querySelector("#theGrid");
+    for (var j = 0; j < theMatrixParent.children.length;j++){
+        var subElement = theMatrixParent.children[i];
+        subElement.addEventListener('click',clickSelector,false);
+    } // end of the grid selector
+
+function clickSelector(e){
+    
+    console.log(e.srcElement.id);
+    var value = e.srcElement.id;
+    tickMatrix.push(value);
+    console.log(tickMatrix)
+    
+    if(whosMove == 0){
+        
+        var maximum = 9;
+        var minimum = 1;
+        var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+        console.log(randomnumber);
+        
+        for (var k = 0; k < tickMatrix.length;k++){
+            
+            if(randomnumber == k[i]){
+                
+                randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+                console.log( "new number: " + randomnumber);
+                
+            } else {
+                console.log("in theloop")
+                document.getElementById(randomnumber).innerHTML = computerSelection;
+                tempArray.push(randomnumber);
+                console.log(tempArray);
+                whosMove = 0;
+                console.log("in matrix:" + whosMove);
+                k = tickMatrix.length;
+                
+            }
+            console.log(tempArray);
+            tickMatrix.concat(tempArray);
+            console.log(tickMatrix);
+            tempArray = [];
+            
+        }//random click element
+        
+    }  //end of if statment
+    
+    
+    
+    
+}    //grid click selector end
+    
 	
-	
-function theGame(value){
+/*function theGame(value){
      switch(value){
         case '1':
              console.log("value of whosMoveL: " + whosMove);
              console.log("value of userSelection in the Game: " + userSelection)      
              if(whosMove == 0){     
-                tickMatrix.push('1',userSelection); 
+                tickMatrix.push('1'); 
                 console.log(tickMatrix);
                 whosMove = 1; 
 	console.log("whois 0 " + computerSelection) 
@@ -67,17 +117,130 @@ function theGame(value){
                  whosMove = 0;
              }
             break;//end of case 1
-             
-     
-             
-             
-             
-             
-             
+         case '2':
+             console.log("value of whosMoveL: " + whosMove);
+             console.log("value of userSelection in the Game: " + userSelection)      
+             if(whosMove == 0){     
+                tickMatrix.push('2'); 
+                console.log(tickMatrix);
+                whosMove = 1; 
+	console.log("whois 0 " + computerSelection) 
+             } else if(whosMove == 1) {
+                 tickMatrix.push('2')
+                 console.log(tickMatrix);
+                 whosMove = 0;
+             }
+            break;//end of case 2     
+      case '3':
+             console.log("value of whosMoveL: " + whosMove);
+             console.log("value of userSelection in the Game: " + userSelection)      
+             if(whosMove == 0){     
+                tickMatrix.push('3'); 
+                console.log(tickMatrix);
+                whosMove = 1; 
+	console.log("whois 0 " + computerSelection) 
+             } else if(whosMove == 1) {
+                 tickMatrix.push('3')
+                 console.log(tickMatrix);
+                 whosMove = 0;
+             }
+            break;//end of case 3
+              case '4':
+             console.log("value of whosMoveL: " + whosMove);
+             console.log("value of userSelection in the Game: " + userSelection)      
+             if(whosMove == 0){     
+                tickMatrix.push('4'); 
+                console.log(tickMatrix);
+                whosMove = 1; 
+	console.log("whois 0 " + computerSelection) 
+             } else if(whosMove == 1) {
+                 tickMatrix.push('4')
+                 console.log(tickMatrix);
+                 whosMove = 0;
+             }
+            break;//end of case 4
+              case '5':
+             console.log("value of whosMoveL: " + whosMove);
+             console.log("value of userSelection in the Game: " + userSelection)      
+             if(whosMove == 0){     
+                tickMatrix.push('5'); 
+                console.log(tickMatrix);
+                whosMove = 1; 
+	console.log("whois 0 " + computerSelection) 
+             } else if(whosMove == 1) {
+                 tickMatrix.push('5')
+                 console.log(tickMatrix);
+                 whosMove = 0;
+             }
+            break;//end of case 5
+              case '6':
+             console.log("value of whosMoveL: " + whosMove);
+             console.log("value of userSelection in the Game: " + userSelection)      
+             if(whosMove == 0){     
+                tickMatrix.push('6'); 
+                console.log(tickMatrix);
+                whosMove = 1; 
+	console.log("whois 0 " + computerSelection) 
+             } else if(whosMove == 1) {
+                 tickMatrix.push('6')
+                 console.log(tickMatrix);
+                 whosMove = 0;
+             }
+            break;//end of case 6            
+               case '7':
+             console.log("value of whosMoveL: " + whosMove);
+             console.log("value of userSelection in the Game: " + userSelection)      
+             if(whosMove == 0){     
+                tickMatrix.push('7'); 
+                console.log(tickMatrix);
+                whosMove = 1; 
+	console.log("whois 0 " + computerSelection) 
+             } else if(whosMove == 1) {
+                 tickMatrix.push('7')
+                 console.log(tickMatrix);
+                 whosMove = 0;
+             }
+            break;//end of case 7            
+               case '8':
+             console.log("value of whosMoveL: " + whosMove);
+             console.log("value of userSelection in the Game: " + userSelection)      
+             if(whosMove == 0){     
+                tickMatrix.push('8'); 
+                console.log(tickMatrix);
+                whosMove = 1; 
+	console.log("whois 0 " + computerSelection) 
+             } else if(whosMove == 1) {
+                 tickMatrix.push('8')
+                 console.log(tickMatrix);
+                 whosMove = 0;
+             }
+            break;//end of case 8            
+                 case '9':
+             console.log("value of whosMoveL: " + whosMove);
+             console.log("value of userSelection in the Game: " + userSelection)      
+             if(whosMove == 0){     
+                tickMatrix.push('5'); 
+                console.log(tickMatrix);
+                whosMove = 1; 
+	console.log("whois 0 " + computerSelection) 
+             } else if(whosMove == 1) {
+                 tickMatrix.push('9')
+                 console.log(tickMatrix);
+                 whosMove = 0;
+             }
+            break;//end of case 9                   
              
                  }//end of switch statement
-}//end of the gamefunction
+}*///end of the gamefunction
     
+
+function computerLogic(){
+    
+    
+    
+    
+    
+}    
     
     
 
