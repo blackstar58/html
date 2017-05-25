e76ed1330020c2307e52e0bdf7a3bf50a93fd9c5
Ahmed Syed <blackstar58@gmail.com>
2017-05-24 17:01:14 -0700

    May 24th Update
    - Removed tick, tick.js
    - Added Simon starter file

--- ticktoe/tick.js
+++ ticktoe/tick.js
@@ -2,37 +2,34 @@ $(window).on('load', function(){
 $('#myModal').modal('show');	
 });
 
-$(document).ready(function (){  
-	
-var userSelection = null;
-var computerSelection = null;
-var tickMatrix = [];
-var userMoves = [];
-var computerMoves = [];	
-var winningMoves = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-var randomNumber = 0;
 
-    
-//selection logic
-
-function displayChoice(rand){
-	document.getElementById(rand).innerHTML = computerSelection;
-	tickMatrix.push(rand);
-	computerMoves.push(rand);
-}	//display the computers choice
+$(document).ready(function (){ 
 	
+	var userSelection = null;
+	var computerSelection = null;
+	var tickMatrix = [];
+	var userMoves = [];
+	var computerMoves = [];	
+	var winningMoves = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
 $('#selectX').on("click",function(){	
 	userSelection = 'X';
 	computerSelection = 'O';
-    $('#myModal').modal('hide');
-	})//end of select X function
+	$('#myModal').modal('hide');
+		})//end of select X function
 	
 $('#selectO').on("click",function(){		
 	userSelection = 'O';
 	computerSelection = 'X';
-    $('#myModal').modal('hide');
- 												Attention
-	})//end of select O function	
+    $('#myModal').modal('hide'); 	
+	nextMoves();
+	})//end of select O
+
+$('#restart').on("click",function(){		
+    $('#compWinModel').modal('hide'); 	
+		clearScreen('tie');
+	})//end of restart
+
                
 var theParent = document.querySelector("#workArea");
   for (var i = 0; i < theParent.children.length; i++) {
@@ -40,38 +37,116 @@ var theParent = document.querySelector("#workArea");
     childElement.addEventListener('click', clickedElement, false);
 }//end of click listener
 function clickedElement(e){
-	var value = e.srcElement.id;
-    e.path[0].innerHTML = userSelection;    
-}	//end of clickElement Modifier
-    var theMatrixParent = document.querySelector("#theGrid");
-    	for (var j = 0; j < theMatrixParent.children.length;j++){
-        var subElement = theMatrixParent.children[i];
-        subElement.addEventListener('click',userAssign,false);
-    } // end of the grid selector
- 
-	
-function userAssign(e){
-    console.log(e.srcElement.id);
+     var lock = parseInt(e.path[0].id);
+     if(tickMatrix.indexOf(lock) < 0){
     var value = parseInt(e.srcElement.id);
-    tickMatrix.push(value);
+	tickMatrix.push(value);
 	userMoves.push(value);
-}   
+	e.path[0].innerHTML = userSelection;     
+    nextMoves();//computer Move
+        
+    }
+    
+  }	//end of clickElement Modifier
+
+function clearScreen(value){
 	
-function clearScreen(){
-$('#myModal').modal('show');	 
+if(value == 'tie'){
+	$('#myModal').modal('show');	 
 	for(var k=1;k <=9;k++){
            document.getElementById(k).innerHTML = "";
            tickMatrix = [];
 		   userMoves = [];
 		   computerMoves = [];
-        }  //end of reset for loop       
-}//end of clear
+        }  //end of reset for loop  
+} else if(value == 'comp'){
+	
+	$('#compWinModel').modal('show');
+	
+} else if (value=='nomoves'){
+    $('#notice').text('Its a Tie!!');
+    $('#compWinModel').modal('show');
+    
+} else if (value=='userWon'){
+    $('#notice').text('User has Won!!');
+    $('#compWinModel').modal('show');
+     
+}	
+
+}//end of clear	
+	
+function assignSelection(e){
+
+    document.getElementById(e).innerHTML = computerSelection;
+    tickMatrix.push(e);
+	computerMoves.push(e);
+
+}	
+	
+function nextMoves(){
+    
+	if(userMoves.length == 0){
+		assignSelection(5);	
+	} else if (userMoves[0] !=5 && computerMoves.length == 0){
+		assignSelection(5);
+	}else if(userMoves[0] == 5 && computerMoves.length == 0){
+		var min = 1;
+		var max = 4;
+		var firstMove = Math.floor(Math.random() * (max - min))	+ min;
+		assignSelection(firstMove);
+	} else if (tickMatrix.length >= 8) {	
+			   clearScreen('nomoves');
+	}else {
+        var whatIsNext = whoWin();
+        assignSelection(whatIsNext)
+        haveIWon()
+    }
+}	//the computer selection
 	
 	
-	
-	
-})//document ready
+function whoWin(){   
+    var backup = [];
+    var uPossibleMoves = [];
+    var compPossibleMoves = [];
+    for(var t = 0; t < winningMoves.length;t++){
+	   var computerWin = winningMoves[t].filter(isNotInComputer);
+       var userWin = winningMoves[t].filter(isNotInUser);
+        
+        if(computerWin.length == 1 && tickMatrix.indexOf(computerWin[0]) < 0){
+			var selection = (computerWin[0])
+			compPossibleMoves.push(selection);
+        } else if(userWin.length == 1 && tickMatrix.indexOf(userWin[0]) < 0){ //computer wins
+            uPossibleMoves.push(userWin[0])
+        } else if(computerWin.length == 2 && tickMatrix.indexOf(computerWin[0]) < 0 ){   
+            backup.push(computerWin);
+        }
+    
+	   if(compPossibleMoves.length >= 1 && t == 7){		
+			return compPossibleMoves[0];
+		} else if (uPossibleMoves.length >= 1 && t == 7 ){
+			return uPossibleMoves[0];	
+		}else if(t == 7) {
+			return backup[0][0];
+		}
+    }//end of for loop
+}//end of compWin	
 
+function isNotInComputer(value){				
+      return computerMoves.indexOf(value) < 0
+    }     //is Not In end
+function isNotInUser(value){
+       return userMoves.indexOf(value) < 0
+    }     //is Not In end - UserMoves	
 
-
-
+function haveIWon(){
+    for(var m = 0;m < winningMoves.length;m++){    
+        var computerHasWon = winningMoves[m].filter(isNotInComputer);
+        var userHasWon = winningMoves[m].filter(isNotInUser);
+        if(computerHasWon.length == 0){
+            clearScreen('comp');
+        } else if(userHasWon.length == 0){
+            clearScreen('userWon');
+        }//user has won
+    }//end of for loop
+}    //end of have I Won
+})//end of document ready
