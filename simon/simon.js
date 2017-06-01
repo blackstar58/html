c497ef68e42c24860884e4782c532c149ddacfd9
Ahmed Syed <blackstar58@gmail.com>
2017-06-01 16:16:42 -0700

    June 1st Update
    Update to JS script

--- simon/simon.js
+++ simon/simon.js
@@ -1,22 +1,68 @@
 $(document).ready(function (){ 
     
+var userSelection = [];
+var simonPattern = [];
+var onOrOff = false;
+var isItStrict = null;
+
+$('#start').on("click",function(){	
+      console.log("Start Enabled"); 
+    randomNumber();
+		})//end of start
+    
+$('#strict').on("click",function(){ 
+    isItStrict = true;
+      console.log(isItStrict);
+  })//end of Strict
+
+$('#myonoffswitch').on("click",function(){
+    if(onOrOff == false){
+         onOrOff = true;
+        userSelection = [];
+        simonPattern = [];
+        isItStrict = null;
+    console.log("MMMMMM");
+    console.log(onOrOff);  
+    } else if(onOrOff == true){
+        onOrOff = false;
+        console.log(onOrOff);
+    }
+    
+}) //On or Off switch   
+    
+var theParent = document.querySelector("#simonClick");
+  for (var i = 0; i < theParent.children.length; i++) {
+    var childElement = theParent.children[i];
+    childElement.addEventListener('click', clickedElement, false);
+}//end of click listener
+function clickedElement(e){
+if(onOrOff == true){
+         console.log(e.path[0].id);
+    var userClick = e.path[0].id;
+    userSelection.push(userClick);
+    console.log(userSelection);
+        
+    }
+    
+  }	//end of clickElement Modifier
+    
+    
+    
+function randomNumber(){
+    var min = 1;
+		var max = 4;
+		var firstMove = Math.floor(Math.random() * (max - min))	+ min;
+    simonPattern.push(firstMove);
+    console.log(firstMove);
+    console.log(simonPattern);
+} 
+    
+function validation(){
+    
+    
+}    
     
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-} //end of document
+}) //end of document
