b39dc42688e0b05bb22edf96303f5f2b2f71d144
Ahmed Syed <Patt3rnlogic@MacBook-Pro-8.local>
2017-06-04 19:15:57 -0700

    June 4th - Update

--- simon/simon.js
+++ simon/simon.js
@@ -4,10 +4,26 @@ var userSelection = [];
 var simonPattern = [];
 var onOrOff = false;
 var isItStrict = null;
+var count = 0;	
 
-$('#start').on("click",function(){	
-      console.log("Start Enabled"); 
+	
+	
+var redSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");	
+var blueSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+var greenSound = new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+var yellowSound = new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");	
+	
+	
+	
+	
+$('#start').on("click",function(){
+	
+	if(onOrOff == true){
+	clearAll();
+    console.log("Start Enabled"); 
     randomNumber();
+	
+	}
 		})//end of start
     
 $('#strict').on("click",function(){ 
@@ -18,9 +34,7 @@ $('#strict').on("click",function(){
 $('#myonoffswitch').on("click",function(){
     if(onOrOff == false){
          onOrOff = true;
-        userSelection = [];
-        simonPattern = [];
-        isItStrict = null;
+ 		clearAll();
     console.log("MMMMMM");
     console.log(onOrOff);  
     } else if(onOrOff == true){
@@ -39,6 +53,7 @@ function clickedElement(e){
 if(onOrOff == true){
          console.log(e.path[0].id);
     var userClick = e.path[0].id;
+	play(userClick);
     userSelection.push(userClick);
     console.log(userSelection);
         
@@ -46,17 +61,41 @@ if(onOrOff == true){
     
   }	//end of clickElement Modifier
     
-    
+ 
+function clearAll(){
+	userSelection = [];
+	simonPattern = [];
+	isItStrict = null;
+	console.log("All Clear");
+	
+}//clear all elements	
+	
+	
     
 function randomNumber(){
+	
     var min = 1;
 		var max = 4;
 		var firstMove = Math.floor(Math.random() * (max - min))	+ min;
     simonPattern.push(firstMove);
+	play(firstMove);
     console.log(firstMove);
     console.log(simonPattern);
-} 
+} //end of random number
     
+function play(value){
+	
+	if(value == 1){	
+		greenSound.play();
+	} else if(value == 2){
+		redSound.play();
+	} else if(value == 3){
+		yellowSound.play();
+	}else if(value == 4){
+		blueSound.play()
+	}
+}
+
 function validation(){
     
     
